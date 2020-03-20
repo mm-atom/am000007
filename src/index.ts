537aa642f923f94de883325397b5d5ffb3bd3a4d
taoqf <tao_qiufeng@126.com>
2020-03-20 09:28:09 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * @module open-app打开其他软件
+ */
 
-export default function am7(param1: string) {
+import { Linking } from 'react-native';
+
+/**
+ * 拨打电话
+ */
+export default function open_tel(phone_num: string): Promise<boolean> {
+	return Linking.openURL(`tel:${phone_num}`);
 }
